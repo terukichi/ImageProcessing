90a16f181165d833c0fe5f48ffd40398ddecc5d7
terukichi <263539179+terukichi@users.noreply.github.com>
2026-08-11 22:24:37 +0900

    Create IDFT function

--- src/programs/DFT.jl
+++ src/programs/DFT.jl
@@ -10,19 +10,16 @@ module DFT
 
 using ..ImageProcessing: dataLoaded, dataPPM, dataPGM
 
-export dft, DFTtoPGM, amplitudeSpectrum, arrangeMatrix
+export dft, DFTtoPGM, amplitudeSpectrum, arrangeMatrix, idft
 
 mutable struct amplitudeSpectrum
     width::Int
     height::Int
-    max_spc::Float64
-    spectrum::Matrix{Float64}
+    spectrum::Matrix{Complex{Float64}}
 end
 
-function calcDftLog(data::dataPGM, u::Int, v::Int)::Float64
-    cmp::Complex{Float64} = 0.0
-    ans::Float64 = 0.0
-    max_brightness::Float64 = Float64(data.max_brightness)
+function calcDftLog(data::dataPGM, u::Int, v::Int)::Complex{Float64}
+    ans::Complex{Float64} = 0.0
     height::Float64 = Float64(data.height)
     width::Float64 = Float64(data.width)
     for y::Int in 0:height-1
@@ -30,19 +27,18 @@ function calcDftLog(data::dataPGM, u::Int, v::Int)::Float64
         for x::Int in 0:width-1
             xp::Int = x+1
             theta::Float64 = 2pi*(u*x/width+v*y/height)
-            cmp += Float64(data.pixels[yp, xp])/max_brightness * cos(theta)
-            cmp -= Float64(data.pixels[yp, xp])/max_brightness * im*sin(theta)
+            fxy::Float64 = data.pixels[yp, xp]/255.0
+            ans += fxy * cos(theta)
+            ans -= fxy * im*sin(theta)
         end
     end
-    ans = log(abs(cmp))
     return ans
 end
 
 function dft(data::dataPGM)::amplitudeSpectrum
     height::Int = data.height
     width::Int = data.width
-    max_spc::Float64 = 0.0
-    F::Matrix{Float64} = zeros(Float64, height, width)
+    F::Matrix{Complex{Float64}} = zeros(Complex{Float64}, height, width)
     for v::Int in 0:height-1
         vp::Int = v+1
         for u::Int in 0:width-1
@@ -50,18 +46,46 @@ function dft(data::dataPGM)::amplitudeSpectrum
             F[vp, up] = calcDftLog(data, u, v)
         end
     end
-    min_spc = minimum(F)
-    if min_spc < 0
-        F = [x - min_spc for x in F]
-    end
-    max_spc = maximum(F)
 
-    return amplitudeSpectrum(width, height, max_spc, F)
+    return amplitudeSpectrum(width, height,  F)
 end
 
-function EVENxEVEN(data::amplitudeSpectrum)::Matrix{Float64}
-    spectrum::Matrix{Float64} = data.spectrum
-    arranged::Matrix{Float64} = zeros(Float64, data.height, data.width)
+function calcIdft(data::amplitudeSpectrum, x::Int, y::Int)::Float64
+    cmp::Complex{Float64} = 0.0
+    ams::Float64 = 0.0
+    height::Float64 = Float64(data.height)
+    width::Float64 = Float64(data.width)
+    for v::Int in 0:height-1
+        vp::Int = v+1
+        for u::Int in 0:width-1
+            up::Int = u+1
+            theta::Float64 = 2pi*(u*x/width+v*y/height)
+            Fuv::Complex{Float64} = data.spectrum[vp, up]*255.0
+            cmp += Fuv * cos(theta)
+            cmp += Fuv * im*sin(theta)
+        end
+    end
+    ans = abs(cmp/height/width)
+    return ans
+end
+
+function idft(data::amplitudeSpectrum)::dataPGM
+    height::Int = data.height
+    width::Int = data.width
+    f::Matrix{Float64} = zeros(Float64, height, width)
+    for y::Int in 0:height-1
+        yp::Int = y+1
+        for x::Int in 0:width-1
+            xp::Int = x+1
+            f[yp, xp] = calcIdft(data, x, y)
+        end
+    end
+    return dataPGM("P2", width, height, 255, f)
+end
+
+function EVENxEVEN(data::amplitudeSpectrum)::Matrix{Complex{Float64}}
+    spectrum::Matrix{Complex{Float64}} = data.spectrum
+    arranged::Matrix{Complex{Float64}} = zeros(Complex{Float64}, data.height, data.width)
 
     arranged[1:floor(Int, end/2), 1:floor(Int, end/2)] = spectrum[floor(Int, end/2)+1:end, floor(Int, end/2)+1:end]
     arranged[1:floor(Int, end/2), floor(Int, end/2)+1:end] = spectrum[floor(Int, end/2)+1:end, 1:floor(Int, end/2)]
@@ -70,9 +94,9 @@ function EVENxEVEN(data::amplitudeSpectrum)::Matrix{Float64}
     return arranged
 end
 
-function EVENxODD(data::amplitudeSpectrum)::Matrix{Float64}
-    spectrum::Matrix{Float64} = data.spectrum
-    arranged::Matrix{Float64} = zeros(Float64, data.height, data.width)
+function EVENxODD(data::amplitudeSpectrum)::Matrix{Complex{Float64}}
+    spectrum::Matrix{Complex{Float64}} = data.spectrum
+    arranged::Matrix{Complex{Float64}} = zeros(Complex{Float64}, data.height, data.width)
 
     arranged[1:floor(Int, end/2), 1:floor(Int, end/2)+1] = spectrum[floor(Int, end/2)+1:end, floor(Int, end/2)+1:end]
     arranged[1:floor(Int, end/2), floor(Int, end/2)+2:end] = spectrum[floor(Int, end/2)+1:end, 1:floor(Int, end/2)]
@@ -81,9 +105,9 @@ function EVENxODD(data::amplitudeSpectrum)::Matrix{Float64}
     return arranged
 end
 
-function ODDxEVEN(data::amplitudeSpectrum)::Matrix{Float64}
-    spectrum::Matrix{Float64} = data.spectrum
-    arranged::Matrix{Float64} = zeros(Float64, data.height, data.width)
+function ODDxEVEN(data::amplitudeSpectrum)::Matrix{Complex{Float64}}
+    spectrum::Matrix{Complex{Float64}} = data.spectrum
+    arranged::Matrix{Complex{Float64}} = zeros(Complex{Float64}, data.height, data.width)
 
     arranged[1:floor(Int, end/2)+1, 1:floor(Int, end/2)] = spectrum[floor(Int, end/2)+1:end, floor(Int, end/2)+1:end]
     arranged[floor(Int, end/2)+2:end, 1:floor(Int, end/2)] = spectrum[1:floor(Int, end/2), floor(Int, end/2)+1:end]
@@ -92,9 +116,9 @@ function ODDxEVEN(data::amplitudeSpectrum)::Matrix{Float64}
     return arranged
 end
 
-function ODDxODD(data::amplitudeSpectrum)::Matrix{Float64}
-    spectrum::Matrix{Float64} = data.spectrum
-    arranged::Matrix{Float64} = zeros(Float64, data.height, data.width)
+function ODDxODD(data::amplitudeSpectrum)::Matrix{Complex{Float64}}
+    spectrum::Matrix{Complex{Float64}} = data.spectrum
+    arranged::Matrix{Complex{Float64}} = zeros(Complex{Float64}, data.height, data.width)
 
     arranged[1:floor(Int, end/2)+1, 1:floor(Int, end/2)+1] = spectrum[floor(Int, end/2)+1:end, floor(Int, end/2)+1:end]
     arranged[1:floor(Int, end/2)+1, floor(Int, end/2)+2:end] = spectrum[floor(Int, end/2)+1:end, 1:floor(Int, end/2)]
@@ -106,9 +130,8 @@ end
 function arrangeMatrix(data::amplitudeSpectrum)::amplitudeSpectrum
     height::Int = data.height
     width::Int = data.width
-    max_spc::Float64 = data.max_spc
-    spectrum::Matrix{Float64} = data.spectrum
-    arranged::Matrix{Float64} = zeros(Float64, height, width)
+    spectrum::Matrix{Complex{Float64}} = data.spectrum
+    arranged::Matrix{Complex{Float64}} = zeros(Complex{Float64}, height, width)
 
     if iseven(height)
         if iseven(width)
@@ -123,15 +146,19 @@ function arrangeMatrix(data::amplitudeSpectrum)::amplitudeSpectrum
             arranged = ODDxODD(data)
         end
     end
-    return amplitudeSpectrum(width, height, max_spc, arranged)
+    return amplitudeSpectrum(width, height,  arranged)
 end
 
 function DFTtoPGM(data::amplitudeSpectrum)::dataPGM
-    F_log::Matrix{Float64} = zeros(Float64, data.height, data.width)
+    F::Matrix{Float64} = log.(abs.(data.spectrum))
     F_normalized::Matrix{Float64} = zeros(Float64, data.height, data.width)
-    if data.max_spc > 0
-        F_log = [x / data.max_spc for x in data.spectrum]
-        F_normalized = [x / data.max_spc * 255 for x in data.spectrum]
+    min_spc = minimum(F)
+    if min_spc < 0
+        F = [x - min_spc for x in F]
+    end
+    max_spc = maximum(F)
+    if max_spc > 0
+        F_normalized = [x / max_spc * 255.0 for x in F]
     end
     pixels::Matrix{Int16} = round.(Int16, F_normalized)
     return dataPGM("P2", data.width, data.height, 255, pixels)
